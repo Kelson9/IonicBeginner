0616caf1098fa8ba5ac5e2c7d24d95507c88239f
Ngouleu-Tertullien <ngouleutert@gmail.com>
2020-11-17 18:32:30 +0100

    bugs and fixes

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
      },
      {
         path:':placeId',
-        loadChildren:'./offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+        loadChildren:'./offers/offeroffer-bookings.module#OfferBookingsPageModule'
 
  }
 

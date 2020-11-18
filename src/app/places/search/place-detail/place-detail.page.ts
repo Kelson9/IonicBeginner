432cdd580fe962617d5f2838e809b0605aef7b83
Ngouleu-Tertullien <ngouleutert@gmail.com>
2020-11-18 18:26:17 +0100

    successfully implimented authguards

--- src/app/places/search/place-detail/place-detail.page.ts
+++ src/app/places/search/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,14 +11,27 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router:Router, private navCtrl:NavController ) { }
-
+  constructor(private router:Router, private navCtrl:NavController,private modalCtrl:ModalController ) { }
+place:Place;
   ngOnInit() {
   }
 
   onBookPlace(){
 
-    this.router.navigateByUrl('/places/tabs/search');
-    this.navCtrl.navigateBack('/places/tabs/search');
+    // this.router.navigateByUrl('/places/tabs/search');
+    // this.navCtrl.navigateBack('/places/tabs/search');
+  this.modalCtrl.create({component:CreateBookingComponent,componentProps:{selectedPlace:this.place}})
+  .then(modalEl=>{
+    modalEl.present();
+    modalEl.onDidDismiss();
+  // })
+//   .then(resultData=>{
+// console.log(resultData.data,resultData.role);
+// if(resultData.role==='confirm'){
+//   console.log('BOOKED'); 
+// }
+  });
+
+  
   }
 }
